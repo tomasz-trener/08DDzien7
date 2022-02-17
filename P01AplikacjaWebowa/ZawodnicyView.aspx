a54d59da6273b8d9e0e7e3dfceff73595215c2a9
kurs <kurs@DESKTOP-V3HDKPQ>
2022-02-17 09:20:05 +0100

    Add project files.

---
+++ P01AplikacjaWebowa/ZawodnicyView.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ZawodnicyView.aspx.cs" Inherits="P01AplikacjaWebowa.ZawodnicyView" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+        <asp:Button ID="btnWczytaj" OnClick="btnWczytaj_Click" runat="server" Text="Wczytaj" />
+
+        <asp:ListBox ID="lbDane" runat="server"></asp:ListBox>
+
+    </form>
+</body>
+</html>
---
+++ P01AplikacjaWebowa/ZawodnicyView.aspx.cs
@@ -0,0 +1,36 @@
+﻿using P04AplikacjaZawodnicy.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace P01AplikacjaWebowa
+{
+    public partial class ZawodnicyView : System.Web.UI.Page
+    {
+        IDostepDoDanych iDostepDoDanych;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnWczytaj_Click(object sender, EventArgs e)
+        {
+            iDostepDoDanych = new ZawodnicyRepository();
+            Odswiez();
+        }
+
+        private void Odswiez()
+        {
+            Zawodnik.KolumnyZWidoku = new string[]{ "Imie","Nazwisko" };
+            Zawodnik[] zawodnicy = iDostepDoDanych.WygenerujZawodnikow();
+            lbDane.DataSource = zawodnicy;
+            lbDane.DataTextField = "WidoczneKolumny";
+         //   lbDane.DataValueField = "Id_zawodnika";
+            lbDane.DataBind();
+
+        }
+    }
+}
